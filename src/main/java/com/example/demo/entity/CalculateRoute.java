bc3e8950dd3bad338cf817d96ba4b2b69907e9f2
Maciek <maciej.wyzlinski@gmail.com>
2018-11-25 01:28:53 +0100

    Poczatek logiki

---
+++ src/main/java/com/example/demo/entity/CalculateRoute.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity;
+
+import java.util.ArrayList;
+
+public class CalculateRoute {
+    public void route(Crossroad start, Crossroad finish) {
+        ArrayList<Crossroad> visited = new ArrayList<>();
+        visited.add(start);
+        
+    }
+}
