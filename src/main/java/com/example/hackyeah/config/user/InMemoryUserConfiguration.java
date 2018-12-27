6f49da359580305c95eee06de7b616c52e5a69df
Ajris <hubiflower@gmail.com>
2018-12-27 03:08:19 +0100

    small fix to security

---
+++ src/main/java/com/example/hackyeah/config/user/InMemoryUserConfiguration.java
@@ -0,0 +1,30 @@
+package com.example.hackyeah.config.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class InMemoryUserConfiguration {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public InMemoryUserConfiguration(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user").password(passwordEncoder.encode("password"))
+                .roles("USER").build());
+        manager.createUser(User.withUsername("admin").password(passwordEncoder.encode("admin"))
+                .roles("USER", "ADMIN").build());
+        return manager;
+    }
+}
